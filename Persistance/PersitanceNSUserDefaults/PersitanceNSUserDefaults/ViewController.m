ed3dc12512c232b6bd3f24ac88460e7bf6dd212c
Tibor Udvari <tibor.udvari@he-arc.ch>
2013-06-03 14:01:49 +0200

    Finished NSUserDefaultsRecipe

--- Persistance/PersitanceNSUserDefaults/PersitanceNSUserDefaults/ViewController.m
+++ Persistance/PersitanceNSUserDefaults/PersitanceNSUserDefaults/ViewController.m
@@ -17,7 +17,59 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.firstNameTextField.delegate = self;
+    self.lastNameTextField.delegate = self;
+    
+    [self loadPersistantData:self];
+    
+	[[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(savePersistentData:) name:UIApplicationDidEnterBackgroundNotification object:nil];
+}
+
+-(BOOL) textFieldShouldReturn:(UITextField *)textField
+{
+    [textField resignFirstResponder];
+    return NO;
+}
+
+-(void) savePersistentData: (id) sender
+{
+    NSUserDefaults* userDefaults = [NSUserDefaults standardUserDefaults];
+    
+    // Set Object/ Value to persist
+    [userDefaults setObject:self.firstNameTextField.text forKey:@"firstName"];
+    [userDefaults setObject:self.lastNameTextField forKey:@"lastName"];
+    [userDefaults setBool:self.activitySwitch.on forKey:@"activityOn"];
+    
+    // Save changes
+    [userDefaults synchronize];
+}
+
+-(void) loadPersistantData : (id) sender
+{
+    NSUserDefaults* userDefaults = [NSUserDefaults standardUserDefaults];
+
+    self.firstNameTextField.text = [userDefaults objectForKey:@"firstName"];
+    self.lastNameTextField.text = [userDefaults objectForKey:@"lastName"];
+    self.activitySwitch.on = [userDefaults boolForKey:@"activityOn"];
+    
+    if (self.activitySwitch.on)
+    {
+        [self.activityIndicator startAnimating];
+    }
+    
+}
+
+-(void) toggleActivity:(id)sender
+{
+    if (self.activitySwitch.on)
+    {
+        [self.activityIndicator startAnimating];
+    }
+    else
+    {
+        [self.activityIndicator stopAnimating];
+    }
 }
 
 - (void)didReceiveMemoryWarning
@@ -25,5 +77,6 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
 
 @end
